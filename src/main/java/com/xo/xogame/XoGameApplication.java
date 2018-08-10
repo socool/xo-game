951497fd758d0d7d2615c17edcb0c4356309fd41
Kamolsit Mongkolsrisawat <socool@Kamolsits-MacBook-Pro.local>
2018-08-10 17:10:14 +0700

    first commit

---
+++ src/main/java/com/xo/xogame/XoGameApplication.java
@@ -0,0 +1,12 @@
+package com.xo.xogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XoGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XoGameApplication.class, args);
+	}
+}
